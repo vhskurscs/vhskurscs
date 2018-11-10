9d1dad566661448f2372056d7ed07fc696531b3c
Marvin Pokroppa <marvin@tygaware.de>
2018-11-10 01:33:48 +0100

    Added calling static methods in Methoden.cs

--- Methoden/Methoden.cs
+++ Methoden/Methoden.cs
@@ -10,6 +10,19 @@ namespace Methoden
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Methoden");
+
+            //Methode aufrufen
+            double ergebnis = Rechner.Addieren(12.0, 11.5);
+
+            Console.WriteLine(Rechner.Addieren(1.0, 2.0, 45.0));
+
+            //Console.WriteLine("Ergebnis: " + ergebnis);
+
+            Rechner.Print("Ergebnis: " + ergebnis);
+
+            Console.WriteLine("Press enter to exit...");
+            Console.ReadLine();
         }
     }
 }
